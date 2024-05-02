3072f3575c0e896f5fd503e2b23a641691ed4e79
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2024-05-01 23:26:15 -0700

    font

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar"
 import HeroSection from "./components/HeroSection/HeroSection";
 import Footer from "./components/Footer/Footer";
-import './App.css'; // If using CSS
 // import ServicesSection from './components/ServicesSection/ServicesSection'
 import GallerySection from "./components/GallerySection/GallerySection";
 import ScheduleSection from "./components/ScheduleSection";
