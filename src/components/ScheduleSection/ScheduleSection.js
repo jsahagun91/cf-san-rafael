211b9bfbfec18c36bef61aacf1c7f9025aff180b
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2024-05-02 21:00:49 -0700

    gallery

--- src/components/ScheduleSection/ScheduleSection.js
+++ src/components/ScheduleSection/ScheduleSection.js
@@ -28,7 +28,7 @@ function ScheduleSection() {
         
         // Set initial position with a delay to ensure layout is updated
         setTimeout(() => {
-            controls.start({ x: initialXOffset + 30 });
+            controls.start({ x: initialXOffset + 50 });
         }, 100);
     }, [controls]);
 
