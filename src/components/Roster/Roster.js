5a152d89a513935cbf88abc44804b01a404a8762
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2024-05-10 13:03:32 -0700

    roster update

--- src/components/Roster/Roster.js
+++ src/components/Roster/Roster.js
@@ -6,6 +6,18 @@ const players = [
     {name: "Diego G. Gonzalez", jerseyNumber: 20, position: "Defense", nationality: "🇲🇽"},
     {name: "Wilson S. Barahona", jerseyNumber: 20, position: "Forward", nationality: "🇺🇸"},
     {name: "Angel Hidalgo", jerseyNumber: 20, position: "Midfield", nationality: "🇺🇸"},
+    {name: "Dave A. Mazariegos", jerseyNumber: 20, position: "Midfield", nationality: "🇺🇸"},
+    {name: "Brennan R. Blondin", jerseyNumber: 20, position: "Forward", nationality: "🇺🇸"},
+    {name: "Leonel R. De Leon", jerseyNumber: 20, position: "Defense", nationality: "🇬🇹"},
+    {name: "Adrian D. Pleitaz", jerseyNumber: 20, position: "Forward", nationality: "🇸🇻"},
+    {name: "Keven Nguyen", jerseyNumber: 20, position: "Midfield", nationality: "🇺🇸"},
+    {name: "Jonathan A. Villeda", jerseyNumber: 20, position: "Defense", nationality: "🇺🇸"},
+    {name: "Dennis A. Hernandez", jerseyNumber: 20, position: "Forward", nationality: "🇸🇻"},
+    {name: "Luis D. Pleitez", jerseyNumber: 20, position: "Defense", nationality: "🇸🇻"},
+    {name: "Santiago Perez", jerseyNumber: 20, position: "Midfield", nationality: "🇲🇽"},
+    {name: "Omar Garcia", jerseyNumber: 20, position: "Defense", nationality: "🇲🇽"},
+    {name: "Jose T. Yama Ortiz", jerseyNumber: 20, position: "Goalkeeper", nationality: "🇲🇽"},
+    {name: "Jhovani I. Barrera", jerseyNumber: 20, position: "Forward", nationality: "🇲🇽"},
 
 ];
 
